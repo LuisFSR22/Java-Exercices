6ab690d2a77748fafffe6be152722416e4fb08a7
LuisFSR22 <93679720+LuisFSR22@users.noreply.github.com>
2023-11-12 23:53:16 +0000

    Create app file

---
+++ arrays_and_linked_lists/App.java
@@ -0,0 +1,10 @@
+package arrays_and_linked_lists;
+
+import arrays_and_linked_lists.resources.menus.main.Main;;
+
+public class App {
+    
+    public static void main(String[] args) {
+        Main.menuInterface();
+    }
+}
